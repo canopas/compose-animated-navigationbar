e88984454398c38633cd1c30cb998456500b85b5
cp-megh <megh.l@canopas.com>
2024-01-03 18:37:34 +0530

    Make worm indicator internal

--- bottombar/src/main/java/com/example/bottombar/utils/ModifierExtensions.kt
+++ bottombar/src/main/java/com/example/bottombar/utils/ModifierExtensions.kt
@@ -3,6 +3,12 @@ package com.example.bottombar.utils
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.RoundRect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.unit.Dp
 
 internal fun Modifier.conditional(
     condition: Boolean,
@@ -13,5 +19,31 @@ internal fun Modifier.conditional(
         modifier.invoke(this)
     } else {
         orElse?.invoke(this) ?: this
+    }
+}
+
+internal fun Modifier.customWormTransition(
+    offset: Dp,
+    indicatorColor: Color,
+    itemWidth: Dp
+) = composed {
+    drawWithContent {
+        val distance = itemWidth.roundToPx()
+        val scrollPosition = (offset.toPx().div(distance))
+        val wormOffset = (scrollPosition % 1) * 2
+
+        val xPos = scrollPosition.toInt() * distance
+        val head = xPos + distance * 0f.coerceAtLeast(wormOffset - 1)
+        val tail = xPos + size.width + distance * 1f.coerceAtMost(wormOffset)
+
+        val worm = RoundRect(
+            head, 0f, tail, size.height,
+            CornerRadius(50f)
+        )
+
+        val path = Path().apply { addRoundRect(worm) }
+
+        // Draw the worm shape on top of the content
+        drawPath(path = path, color = indicatorColor)
     }
 }
